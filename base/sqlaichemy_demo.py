5198afb87aa8364dbb762584f84d0c1603c7080d
johu <yewn@ipampas.com>
2018-01-29 17:39:08 +0800

    sqlaichemy demo

--- base/sqlaichemy_demo.py
+++ base/sqlaichemy_demo.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # 创建对象的基类:
 Base = declarative_base()
 
+
 # 定义User对象:
 class User(Base):
     # 表的名字:
@@ -14,6 +15,7 @@ class User(Base):
     # 表的结构:
     id = Column(String(20), primary_key=True)
     name = Column(String(20))
+
 
 # 初始化数据库连接:
 engine = create_engine('mysql+mysqlconnector://root:123456@10.10.100.20:3306/metadata')
@@ -34,7 +36,7 @@ session.close()
 # 创建Session:
 session = DBSession()
 # 创建Query查询，filter是where条件，最后调用one()返回唯一行，如果调用all()则返回所有行:
-user = session.query(User).filter(User.id=='5').one()
+user = session.query(User).filter(User.id == '5').one()
 # 打印类型和对象的name属性:
 print('type:', type(user))
 print('name:', user.name)
